e7a05b341f392cb8920bb837f630be2e043f5c7b
TS22082 <ts22082@gmail.com>
2020-01-07 20:28:01 -0800

    modify folder structure and add client routes / pages

--- server.js
+++ server.js
@@ -6,14 +6,17 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const postRoutes = require("./controllers/post-routes");
+const postRoutes = require("./controllers/api/post-routes");
 app.use("/api/post", postRoutes);
 
-const authorRoutes = require("./controllers/author-routes");
+const authorRoutes = require("./controllers/api/author-routes");
 app.use("/api/author", authorRoutes);
+
+const clientRoutes = require("./controllers/client/html-routes");
+app.use("/", clientRoutes);
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(`App listening at http://localhost:${PORT}`);
   });
 });
